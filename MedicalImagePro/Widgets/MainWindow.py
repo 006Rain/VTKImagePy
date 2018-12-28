6ad88c23eaf70d330e7e80a490620e20af3be27b
006Rain gengwang000 <gengzw@outlook.com>
2018-12-28 23:36:36 +0800

    Create MainWindow.py

---
+++ MedicalImagePro/Widgets/MainWindow.py
@@ -0,0 +1,12 @@
+#from PyQt5.QtWidgets import ( QMainWindow, QMessageBox, QWidget, QToolTip, QPushButton, QApplication, QLabel, QMenu, QToolButton, QToolBar )
+
+import PyQt5.QWidgets as pwnd
+import PyQt5.QtGui as pgui
+
+class MainWin( pwnd.QMainWindow ):
+	
+	pass
+
+
+
+
